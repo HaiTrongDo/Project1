7ccf52cceb870d14856bf2f36f3d3ba06b133c1f
HaiTrongDo <dotronghai91@gmail.com>
2022-03-28 19:58:37 +0700

    updating self project

--- Js Project/Seft Project/Black Jack Game/Black-Jack-Game.js
+++ Js Project/Seft Project/Black Jack Game/Black-Jack-Game.js
@@ -1,25 +1,51 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let playerEl = document.getElementById("player-el")
+let cardsEl = document.getElementById("cards-el")
+let playerName = "Hai Do"
+let playerChips = 145
 
+playerEl.textContent = playerName + ": $" + playerChips
 // let sumEl = document.querySelector("#sum-el")
 
-let cardsEl = document.getElementById("cards-el")
 
-cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
-sumEl.textContent = "Sum: " + sum;
+
+
+function getRandomCard() {
+    let randomNumber =  Math.floor(Math.random()*13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+
+}
+
+
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
         messageEl.innerText = message;
@@ -36,10 +62,13 @@ function renderGame() {
 
 
 function newCard() {
- console.log("drawing a new card from the desk!")
-    let card = 4;
-    sum += card;
-    renderGame();
-    sumEl.textContent = "Sum: " + sum;
+    if (isAlive === true && hasBlackJack === false) {
+        console.log("drawing a new card from the desk!")
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
+
 }
-// cast out money
